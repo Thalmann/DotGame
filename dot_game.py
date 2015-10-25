72184dbf639daadfb9ddd0e2bb0f9e1cb147b235
Bruno Thalmann <mr.thalmann@gmail.com>
2015-10-25 12:29:57 +0100

    added game loop and  a function that takes mve input

--- dot_game.py
+++ dot_game.py
@@ -1,6 +1,29 @@
 
-dots_count = 13
-def print_dots():
-    print('Number of dots left: {0}'.format(dots_count)) 
+def print_dots(dots_count):
+    print('Number of dots left: {0}'.format(dots_count))
 
-print_dots()
+def read_move(player_name):
+    print('{0}: How many dots do you want to remove? (3, 2 or 1)'.format(player_name))
+    while(True):
+        try:
+            move = int(input())
+        except Exception:
+            move = 0
+        if move in range(1, 4):
+            return move
+        print('You have to choose: 1, 2 or 3')
+
+
+def game():
+    dots_count = 13
+    while(True):
+        if dots_count <= 1:
+            # Game over
+            break
+        move = read_move('some_name')
+        dots_count -= move
+
+
+game()
+        
+
